b835a9cce349b80eba8c91a45359aaa6f8aace06
xinqihuang <xinqihuang@qq.com>
2016-12-17 01:37:27 +0800

    regex
    正则表达式练习

---
+++ Regex/src/edu/huangxinqi/regex/RegexDemo.java
@@ -0,0 +1,24 @@
+package edu.huangxinqi.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by huangxinqi on 2016/12/17/0017.
+ */
+public class RegexDemo {
+    private static final String REGEX ="/w";
+
+    public static void main(String[] args) {
+        System.out.println("please input your regex");
+        Scanner scanner=new Scanner(System.in);
+        String regex =scanner.nextLine();
+        System.out.println("please input your example");
+        String example =scanner.nextLine();
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(example);
+        System.out.println(matcher.find());
+
+    }
+}
